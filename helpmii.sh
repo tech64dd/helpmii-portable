7b8eb325f0140764c6fea5c59308e8fa86fce796
techflashYT <officialTechflashYT@gmail.com>
2024-10-28 13:55:55 -0700

    convert helpmii to use the common dialog utilities

--- helpmii.sh
+++ helpmii.sh
@@ -1,81 +1,54 @@
 #!/bin/bash
 
-VERSION="v1.0"
-DIALOG_COMMON="--no-mouse"
-DIALOG_CANCEL=1
-DIALOG_ESC=255
-HEIGHT=0
-WIDTH=0
+export VERSION="v1.1"
 
-left_text="HelpMii Wii Linux Support Program"
-right_text="$VERSION"
+export left_text="HelpMii Wii Linux Support Program"
+export right_text="$VERSION"
 
-text_len=$((${#left_text} + ${#right_text}))
-# dialog has 2 blank chars on the left and right
-padding_length=$((COLUMNS - text_len - 2))
-padding=$(printf "%${padding_length}s")
-
-BACKTITLE="$left_text$padding$right_text"
-unset text_len padding_length padding left_text right_text
-
-
+if [ -f ../util-dialog.sh ]; then
+	. ../util-dialog.sh
+elif [ -f ../common-util-scripts/util-dialog.sh ]; then
+	. ../common-util-scripts/util-dialog.sh
+else
+	echo "failed to load util-dialog.sh"
+	exit 1
+fi
 
 while true; do
-  exec 3>&1
-  selection=$(dialog $DIALOG_COMMON \
-    --backtitle "$BACKTITLE" \
-    --title "Main Menu" \
-    --clear \
-    --cancel-label "Cancel" \
-    --menu "Select an option:" $HEIGHT $WIDTH 4 \
-    "1" "Join the Wii Linux Discord" \
-    "2" "Get Started guide" \
-    "3" "Upload logs" \
-    "4" "Initiate Reverse Shell (!!!)" \
-    "0" "Quit" \
-    2>&1 1>&3)
-  exit_status=$?
-  exec 3>&-
-  case $exit_status in
-    $DIALOG_CANCEL)
-      clear
-      exit
-      ;;
-    $DIALOG_ESC)
-      clear
-      exit 1
-      ;;
-  esac
-  case $selection in
-    1 )
-      clear
-      cat discord_qr.txt
-      read -n 1 -s -r -p "Press any key to continue...";printf "\n"
-      ;;
-    2 )
-      clear
-      less ./getstarted.txt
-      ;;
-    3 )
-      clear
-      ./dumplogs.sh 2>&1 | gzip > /tmp/logs.txt.gz
-      printf "Uploading logs, please wait...\n"
-      curl bashupload.com -s -T /tmp/logs.txt.gz
-      rm -f /tmp/logs.txt.gz
-      printf "Show the text above to whoever is helping you in the Wii Discord server\n"
-      read -n 1 -s -r -p "Press any key to continue...";printf "\n"
-      ;;
-    4 )
-      dialog $DIALOG_COMMON --title "WARNING-WARNING-WARNING!" --yesno "You are about to give the admins of Wii Linux complete\nand total control over your Wii.\nNobody here will look through your files or the like\nbut, the shell will be running as root, so in theory, they could (but again, they shouldn't).\n\nAre you sure you want to do this?" 12 60
-      response=$?
-      case $response in
-        0) exec ./reverseshell.sh;;
-        1) clear;;
-      esac
-      ;;
-    0 )
-      clear
-      exit
-      ;;
-  esac
+	menu "Main Menu" \
+	"OK" "Cancel" \
+	"Select an option:" \
+	"1" "Join the Wii Linux Discord" \
+	"2" "Read the getting started guide" \
+	"3" "Upload logs to Wii Linux support" \
+	"4" "Initiate reverse shell for Wii Linux support (!!!)" \
+	"9" "About HelpMii" \
+	"0" "Quit"
+	case $? in
+		1)
+			clear
+			cat discord_qr.txt
+			read -n 1 -s -r -p "Press any key to continue..."; echo ;;
+		2)
+			clear
+			less ./getstarted.txt ;;
+		3)
+			clear
+			./dumplogs.sh 2>&1 | gzip > /tmp/logs.txt.gz
+			echo "Uploading logs, please wait..."
+			curl bashupload.com -s -T /tmp/logs.txt.gz
+			rm -f /tmp/logs.txt.gz
+			echo "Show the above texe to whoever is helping you in the Wii Linux Discord server"
+			read -n 1 -s -r -p "Press any key to continue..."; echo ;;
+		4)
+			yesno "WARNING-WARNING-WARNING!" "You are about to give the admins of Wii Linux complete\nand total control over your Wii.\nNobody here will look through your files or the like\nbut, the shell will be running as root, so in theory, they could (but again, they shouldn't).\n\nAre you sure you want to do this?" 12 60
+			case $? in
+				0) ./reverseshell.sh ;;
+				1) clear ;;
+			esac ;;
+		9) info "About" "HelpMii - The Wii Linux Support Program.\n\nHelpMii Version $VERSION\nUsing $UTIL_VER_STR\n\nHelpMii was made by Techflash, Tech64, and other contributors.\nThis program is licensed under the terms of the GNU General Public License, version 2.\nYou may find these terms under the HelpMii install directory, under the LICENSE file." 15 70 ;;
+		0|255)
+			clear
+			exit 0;;
+	esac
 done
--- reverseshell.sh
+++ reverseshell.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-# Add reverse shell stuff here and remove placeholder
-dialog $DIALOG_COMMON --title "Sorry!" --msgbox "This feature isn't implemented yet.\nMake sure your system is up to date by running 'pacman -Syu' as the root user" 7 50
-exec ./helpmii.sh
+unimplemented
