18e69e07c1f52a08263a81f1529f8c35bf8751db
tech64dd <tech64@vivaldi.net>
2025-07-10 12:55:36 +0200

    portable v1.3

--- helpmii.sh
+++ helpmii.sh
@@ -1,53 +1,103 @@
 #!/bin/sh
-
-export VERSION="v1.3"
-
-export left_text="HelpMii Wii Linux Support Program"
-export right_text="$VERSION"
-
-if [ -f ../util-dialog.sh ]; then
-	. ../util-dialog.sh
-elif [ -f ../common-util-scripts/util-dialog.sh ]; then
-	. ../common-util-scripts/util-dialog.sh
-else
-	echo "failed to load util-dialog.sh"
-	exit 1
+#4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI
+#4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paICuKW
+#iOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKW
+#iOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiOKWiAriloji
+#lojilojilogg4paE4paE4paE4paE4paEIOKWiOKWgCDilojiloDiloDilojiloDiloDiloTiloDi
+#lojiloTiloTilogg4paE4paE4paE4paE4paEIOKWiOKWiOKWiOKWiCAgIFNjYW4gdGhpcyBRUiB0
+#byBqb2luCuKWiOKWiOKWiOKWiCDiloggICDilogg4paI4paAIOKWhCDilojilojilojiloTiloDi
+#loDiloQg4paE4paIIOKWiCAgIOKWiCDilojilojilojiloggICB0aGUgRGlzY29yZCBzZXJ2ZXIu
+#CuKWiOKWiOKWiOKWiCDilojiloTiloTiloTilogg4paI4paA4paIIOKWiOKWhCDiloDiloDiloDi
+#loQg4paEIOKWiCDilojiloTiloTiloTilogg4paI4paI4paI4paIICAgICAgICAgIHwK4paI4paI
+#4paI4paI4paE4paE4paE4paE4paE4paE4paE4paI4paE4paI4paE4paIIOKWgOKWhOKWgOKWhOKW
+#iOKWhOKWiOKWhOKWiOKWhOKWhOKWhOKWhOKWhOKWhOKWhOKWiOKWiOKWiOKWiCAgICAgICAgICB8
+#CuKWiOKWiOKWiOKWiOKWhCAgICDiloDiloTiloQgIOKWhOKWiOKWhOKWiOKWhCAg4paE4paIIOKW
+#gCDiloAg4paA4paE4paI4paE4paA4paI4paI4paI4paIICAgPC0tLS0tLeKUmArilojilojiloji
+#lojiloAgIOKWgCDiloDiloTiloTiloQg4paAIOKWhOKWiOKWgOKWhOKWhOKWiOKWgOKWgCDilogg
+#4paE4paA4paA4paI4paA4paI4paI4paI4paI4paICuKWiOKWiOKWiOKWiOKWhCDiloTiloTiloDi
+#loDiloTiloTiloDiloTiloDiloTiloDiloDiloAgIOKWhOKWgCDilojilojiloDiloDiloDiloTi
+#loTilojiloDilojilojilojilogK4paI4paI4paI4paI4paI4paI4paIICDilojiloTiloTiloDi
+#loTilojilojiloAg4paEIOKWiOKWiOKWiOKWiCAg4paA4paE4paA4paE4paE4paA4paI4paI4paI
+#4paI4paIICAgTm90IHNjYW5uaW5nPyAgVHJ5IGFub3RoZXIgYXBwLgrilojilojilojilogg4paE
+#IOKWiCAg4paEIOKWhOKWiOKWhOKWiOKWhOKWiOKWhOKWiOKWgCAgIOKWgOKWgOKWgCDiloDiloQg
+#4paI4paA4paI4paI4paI4paIICAgCuKWiOKWiOKWiOKWiCDilojilogg4paI4paI4paE4paA4paI
+#4paE4paAIOKWhOKWiOKWgCAg4paE4paI4paAIOKWgOKWiOKWhOKWhOKWiOKWhOKWgOKWiOKWiOKW
+#iOKWiOKWiCAgIFN0aWxsIG5vdCBzY2FubmluZywgb3IgZG9uJ3QgaGF2ZQrilojilojilojiloji
+#loTilojiloTilojiloTiloTiloTiloTiloDiloDilojiloTiloDiloDiloDiloDiloDiloDiloji
+#loQg4paE4paE4paEIOKWgCAgIOKWiOKWiOKWiOKWiCAgIGEgZGV2aWNlIHRvIHNjYW4gaXQ/ICAK
+#4paI4paI4paI4paIIOKWhOKWhOKWhOKWhOKWhCDilojiloTiloTiloDilojiloAg4paEIOKWiOKW
+#iCAgIOKWiOKWhOKWiCDiloTiloTilojilojilojilojilojiloggICBVc2UgdGhpcyBsaW5rLgri
+#lojilojilojilogg4paIICAg4paIIOKWiCDiloTiloDilojiloTilojiloTiloDiloDiloTiloDi
+#loAgIOKWhOKWhOKWhOKWgCDiloDiloDilojilojilojilogK4paI4paI4paI4paIIOKWiOKWhOKW
+#hOKWhOKWiCDilogg4paI4paIIOKWhOKWiOKWgCAg4paE4paA4paI4paA4paEIOKWhCAg4paEIOKW
+#iOKWiOKWiOKWiOKWiCAgIGh0dHBzOi8vZGlzY29yZC5nZy9YZk1ITWhTUThkCuKWiOKWiOKWiOKW
+#iOKWhOKWhOKWhOKWhOKWhOKWhOKWhOKWiOKWhOKWiOKWhOKWhOKWiOKWiOKWiOKWiOKWiOKWiOKW
+#iOKWhOKWiOKWhOKWhOKWhOKWhOKWiOKWhOKWiOKWiOKWiOKWiOKWiOKWiArilojilojilojiloji
+#lojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojilojiloji
+#lojilojilojilojilojilojilojilojilojilojilojilojilojilogK4paI4paI4paI4paI4paI
+#4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI
+#4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paI4paICg==
+VERSION="v1.3 Portable"
+DIALOG_COMMON="--no-mouse"
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+left_text="HelpMii Wii Linux Support Program"
+right_text="$VERSION"
+if [ "$COLUMNS" = "" ] || [ "$COLUMNS" = "0" ]; then
+	COLUMNS="$(tput cols)"
 fi
-
-if [ "$UTIL_VER_STR" = "Wii Linux Common Dialog Utilities version 1.0" ]; then
-	printf "\033[1;31mYou have %s, you need v1.1 or later.\033[0m\n" "$UTIL_VER_STR"
-	exit 1
-fi
-
-
+text_len=$((${#left_text} + ${#right_text}))
+# dialog has 2 blank chars on the left and right
+padding_length=$((COLUMNS - text_len - 2))
+padding=$(printf "%${padding_length}s")
+BACKTITLE="$left_text$padding$right_text"
+unset text_len padding_length padding left_text right_text
 while true; do
-	menu "Main Menu" \
-	"OK" "Cancel" \
-	"Select an option:" \
-	"1" "Join the Wii Linux Discord" \
-	"2" "Read the getting started guide" \
-	"3" "Upload logs to Wii Linux support" \
-	"9" "About HelpMii" \
-	"0" "Quit"
-	case $? in
-		1)
-			clear
-			cat discord_qr.txt
-			wait1key "Press any key to continue..."; echo ;;
-		2)
-			clear
-			less ./getstarted.txt ;;
-		3)
-			clear
-			./dumplogs.sh 2>&1 | gzip > /tmp/logs.txt.gz
-			echo "Uploading logs, please wait..."
-			curl bashupload.com -s -T /tmp/logs.txt.gz
-			rm -f /tmp/logs.txt.gz
-			echo "Show the above text to whoever is helping you in the Wii Linux Discord server"
-			wait1key "Press any key to continue..."; echo ;;
-		9) info "About" "HelpMii - The Wii Linux Support Program.\n\nHelpMii Version $VERSION\nUsing $UTIL_VER_STR\n\nHelpMii was made by Techflash, Tech64, and other contributors.\nThis program is licensed under the terms of the GNU General Public License, version 2.\nYou may find these terms under the HelpMii install directory, under the LICENSE file." 15 70 ;;
-		0|255)
-			clear
-			exit 0;;
-	esac
+  exec 3>&1
+  selection=$(dialog $DIALOG_COMMON \
+    --backtitle "$BACKTITLE" \
+    --title "Main Menu" \
+    --clear \
+    --cancel-label "Cancel" \
+    --menu "Select an option:" $HEIGHT $WIDTH 4 \
+    "1" "Join the Wii Linux Discord" \
+    "2" "Read the getting started guide" \
+    "3" "Upload logs to Wii Linux support" \
+    "9" "About HelpMii" \
+    "0" "Quit" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
+  case $exit_status in
+    $DIALOG_CANCEL)
+      clear
+      exit
+      ;;
+    $DIALOG_ESC)
+      clear
+      exit 1
+      ;;
+  esac
+  case $selection in
+    1 )
+      clear
+      tail -n +2 helpmii.sh | head -n 38 | sed 's/#//' | base64 -d
+      read -n 1 -s -r -p "Press any key to continue...";printf "\n"
+      ;;
+    2 )
+      dialog $DIALOG_COMMON --title "Not available" --msgbox "Feature not available in the portable version." 5 50
+      ;;
+    3 )
+      dialog $DIALOG_COMMON --title "Not available" --msgbox "Feature not available in the portable version." 5 50
+      ;;
+    9 )
+      dialog $DIALOG_COMMON --title "About" --msgbox "HelpMii - The Wii Linux Support Program.\n\nHelpMii Version $VERSION\n\nHelpMii was made by Techflash, Tech64, and other contributors.\nThis program is licensed under the terms of the GNU General Public License, version 2.\nYou may find these terms under the HelpMii install directory, under the LICENSE file." 13 70
+      ;;
+    0 )
+      clear
+      exit
+      ;;
+  esac
 done
